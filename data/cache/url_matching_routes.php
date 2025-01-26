44d25b58a8bb8b9ee0c8f2f43818caa64e318f4f
karthikeya-v <vemulapallisaikarthikeya@gmail.com>
2025-01-26 23:17:34 +1000

    reconmap first official commit

---
+++ data/cache/url_matching_routes.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+        '/openapi.json' => [[['_route' => 'reconmap_controllers_system_getopenapiyamlcontroller___invoke', '_controller' => 'Reconmap\\Controllers\\System\\GetOpenApiYamlController::__invoke'], null, null, null, false, false, null]],
+    ],
+    [ // $regexpList
+    ],
+    [ // $dynamicRoutes
+    ],
+    null, // $checkCondition
+];
